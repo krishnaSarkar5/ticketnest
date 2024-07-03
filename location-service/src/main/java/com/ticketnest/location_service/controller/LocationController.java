ea1ab3e8f000a58c6eb935cfea3bd61097e1234d
krishnasarkar95 <krishnasarkar95@gmail.com>
2024-07-03 11:02:08 +0530

    some changes 3

--- location-service/src/main/java/com/ticketnest/location_service/controller/LocationController.java
+++ location-service/src/main/java/com/ticketnest/location_service/controller/LocationController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// controller class
+
 @RestController
 @RequestMapping("/api/v1/location")
 public class LocationController {
